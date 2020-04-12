7ecc3d41bac86e8e81bb1b7e652fe007c47cbb0a
俊佑 郭 <geneissmart@gmail.com>
2020-04-13 01:48:34 +0800

    first commit

--- TaiwanAddress/helpers.js
+++ TaiwanAddress/helpers.js
@@ -8,6 +8,7 @@ function $g(selector) {
     let nodelist = document.querySelectorAll(selector);
     return nodelist.length == 1 ? nodelist[0] : nodelist;
 }
+
 function $c(selector){
     return document.createElement(selector);
 }
